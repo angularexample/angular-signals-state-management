e696cee6f0c6ecb58e34990c953108dfa1a1491b
angularexample <info@angularexample.com>
2025-10-09 09:04:13 -0400

    reorder imports

--- src/app/shared/xxx-header/xxx-header.spec.ts
+++ src/app/shared/xxx-header/xxx-header.spec.ts
@@ -1,7 +1,7 @@
 import { provideRouter } from '@angular/router';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { XxxHeader } from './xxx-header';
 import { XxxContentFacade } from '../../core/xxx-content/xxx-content-facade';
+import { XxxHeader } from './xxx-header';
 
 describe('XxxHeader', () => {
   let fixture: ComponentFixture<XxxHeader>;

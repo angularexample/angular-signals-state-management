439ec31f46b14afcf47b422f2fdfa75d6412347c
angularexample <info@angularexample.com>
2025-10-09 09:05:36 -0400

    reorder imports

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,8 +1,8 @@
+import { App } from './app';
 import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
 import { provideZonelessChangeDetection } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
-import { App } from './app';
-import { provideRouter } from '@angular/router';
 
 describe('App', () => {
   beforeEach(async () => {
